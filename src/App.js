c8d6e5174ed6def85e6c9ab61cfe879303668fa8
saeed <saeedshirini1@gmail.com>
2023-06-13 16:58:36 +0430

    switch component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Login from "./component/Login";
 import Register from "./component/Register";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
+import {Switch} from "react-router-dom"
 
 class App extends Component{
     render(){
         return(
             <>
                 <Navbar/>
-                <Route path="/users" component={Users}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/Register" component={Register}/>
-                <Route path="/" exact component={Home}/>
+                <Switch>
+                    <Route path="/users" component={Users}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/Register" component={Register}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
             </>
         )
     }
