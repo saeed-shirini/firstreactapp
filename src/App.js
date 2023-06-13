b222b5a56d0fbb15244274371cdf960a952aded6
saeed <saeedshirini1@gmail.com>
2023-06-13 13:00:27 +0430

    skeleton loading

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+import Users from "./component/Users.jsx";
+
+class App extends Component{
+    render(){
+        return(
+            <>
+                <Users/>
+            </>
+        )
+    }
+}
+
+export default App
---
+++ src/component/Users.jsx
@@ -0,0 +1,59 @@
+import {Component} from "react";
+import axios from "axios";
+import LoadingUsers from "./loading/loadungUsers.jsx";
+
+class Users extends Component{
+    state={
+        users:[],
+        isLoading:true
+    }
+    async componentDidMount(){
+        let response = await axios.get("https://reqres.in/api/users?page=1");
+        setTimeout(()=>{
+            this.setState({users:response.data.data,isLoading:false})
+        },2000)
+    }
+    render(){
+        return(
+            <div className="users">
+                <button onClick={this.handleCreate} type="button" className="btn btn-info btn-lg">create</button>
+                
+                <div className="container">
+                    <div className="row text-center m-auto">
+                    {this.state.isLoading ? <LoadingUsers/> : (
+                        this.state.users.map((user,index)=>{
+                            return (
+                                <div className="col-md-4 p-5" key={index}>
+                                    <img src={user.avatar} alt="" style={{borderRadius:"50%",width:"100px"}}/>
+                                    <h4>
+                                        {user.first_name} {user.last_name}
+                                    </h4>
+                                    <h5>{user.email}</h5>
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <button onClick={this.handleUpdate} type="button" className="btn btn-info">update</button>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <button onClick={this.handleDelete} type="button" className="btn btn-danger">delete</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }))}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    handleCreate = () => {
+        
+    }
+    handleUpdate = (user) => {
+        
+    }
+    handleDelete = (user) => {
+        
+    }
+}
+
+export default Users
