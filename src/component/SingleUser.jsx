ceb978db587dbea3d93e74c7935d1f50a5878972
saeed <saeedshirini1@gmail.com>
2023-06-15 17:10:11 +0430

    query-string

--- src/component/SingleUser.jsx
+++ src/component/SingleUser.jsx
@@ -1,12 +1,15 @@
 import { useState,useEffect } from "react";
 import axios from "axios";
+import queryString from 'query-string';
 
 const SingleUser = (props) => {
-    let [user,setuser] = useState({})
+    const parse = queryString.parse(props.location.search)
+    console.log(parse)
+    let [user,setuser] = useState({});
     useEffect(async()=>{
         let response = await axios.get(`https://reqres.in/api/users/${props.match.params.id}`);
         setuser(response.data.data);
-    },[])
+    },[]);
     return(
         <>
             <img src={user.avatar} alt="" style={{borderRadius:"50%",width:"100px"}}/>
