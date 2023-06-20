4136946d01f054177ee0f2db2b1093cb50b93610
saeed <saeedshirini1@gmail.com>
2023-06-20 18:45:38 +0430

    controlled input

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,32 +1,37 @@
-import { Component,createRef } from "react";
+import { Component } from "react";
 import axios from "axios";
 
 class Login extends Component {
-    email = createRef();
-    password = createRef();
-
-    handleSubmit = async(e) => {
-        e.preventDefault()
-        const account = {email:this.email.current.value,password:this.password.current.value}
-        if(account.email && account.password){
-            let response = await axios.post("https://reqres.in/api/login",account);
-            console.log(response)
+    state = {
+        account: {
+            email: "",
+            password: ""
         }
     }
+    handleChange = (e) => {
+        let account = {...this.state.account};
+        let {name,value} = e.target;
+        account[name] = value;
+        this.setState({account})
+    }
+    handleSubmit = async (e) => {
+        e.preventDefault()
+    }
     render() {
+        const {email,password} = this.state.account
         return (
             <div className="container mt-3">
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group row">
                         <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
                         <div className="col-sm-10">
-                            <input ref={this.email} type="text" className="form-control" id="email" />
+                            <input onChange={this.handleChange} value={email} type="text" name="email" className="form-control" id="email" />
                         </div>
                     </div>
                     <div className="form-group row">
                         <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
                         <div className="col-sm-10">
-                            <input ref={this.password} type="password" className="form-control" id="password" />
+                            <input onChange={this.handleChange} value={password} type="password" name="password" className="form-control" id="password" />
                         </div>
                     </div>
                     <button className="btn btn-primary">login</button>
