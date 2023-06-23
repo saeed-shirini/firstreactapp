92ff7a9a70db1eb99c65a05a1f2d60ad7c344fb4
saeed <saeedshirini1@gmail.com>
2023-06-23 15:34:46 +0430

    validation

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,30 +1,64 @@
 import { Component } from "react";
 import Input from "./Input";
+import * as yup from "yup";
+import axios from "axios";
 
 class Login extends Component {
     state = {
         account: {
             email: "",
             password: ""
+        },
+        errors: [],
+        sending:false
+    }
+    validate = async () => {
+        const { email, password } = this.state.account;
+        let userSchema = yup.object().shape({
+            email: yup.string().required("email filed is required").email("this filed must be email format"),
+            password: yup.string().required("password filed is required").min(5, "password field dont less 5 char")
+        })
+        try {
+            const result = await userSchema.validate({ email, password }, { abortEarly: false });
+            return result;
+        } catch (err) {
+            this.setState({ errors: err.errors,sending:false })
         }
     }
     handleChange = (e) => {
-        let account = {...this.state.account};
-        let {name,value} = e.target;
+        let account = { ...this.state.account };
+        let { name, value } = e.target;
         account[name] = value;
-        this.setState({account})
+        this.setState({ account })
     }
     handleSubmit = async (e) => {
-        e.preventDefault()
+        e.preventDefault();
+        this.setState({ errors: []});
+        const validate = await this.validate();
+        if (validate) {
+            try {
+                this.setState({sending:true })
+                await axios.post("https://reqres.in/api/login", validate);
+                this.setState({sending:false})
+            } catch (err) {
+                this.setState({ errors: ["email or password is invalid"],sending:false });
+            }
+        }
     }
     render() {
-        const {email,password} = this.state.account
+        const { email, password } = this.state.account;
         return (
             <div className="container mt-3">
+                {this.state.errors.length !== 0 && (
+                    <ul>
+                        {this.state.errors.map((error, index) => <li key={index} className="alert alert-danger" style={{listStyle:"none"}}>{error}</li>
+                        )}
+                    </ul>)
+                }
                 <form onSubmit={this.handleSubmit}>
-                    <Input name="email" onChange={this.handleChange} value={email}/>
-                    <Input name="password" onChange={this.handleChange} value={password}/>
-                    <button className="btn btn-primary">login</button>
+                    <Input name="email" onChange={this.handleChange} value={email} />
+                    <Input name="password" onChange={this.handleChange} value={password} />
+                    <button disabled={this.state.sending} className="btn btn-primary">login</button>
                 </form>
             </div>
         )
