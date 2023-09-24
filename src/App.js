0147d5ab9cedbe8d004bc9a4337769fb233a7669
saeed <saeedshirini1@gmail.com>
2023-09-24 15:07:39 +0330

    add hashRouter to App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./component/Login";
 import Register from "./component/Register";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
-import {Route,Switch,Redirect} from "react-router-dom";
+import {Route,Switch,Redirect,HashRouter} from "react-router-dom";
 import SingleUser from "./component/SingleUser.jsx";
 import NotFound from "./component/NotFound.jsx";
 import Dashboard from "./component/Dashboard";
@@ -26,20 +26,22 @@ class App extends Component{
     render(){
         return(
             <>
-                <Navbar user={this.state.user}/>
-                <Switch>
-                    <Route path="/users/:id" component={SingleUser}/>
-                    <Route path="/users" component={Users}/>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/Register" component={Register}/>
-                    <Route path="/logout" component={Logout}/>
-                    <Protect component={Dashboard} path="/dashboard"/>
-                    <Route path="/dashboard" component={Dashboard}/>
-                    <Route path="/not-found" component={NotFound}/>
-                    <Redirect from="/customers" to="/users"/>
-                    <Route path="/" exact component={Home}/>
-                    <Redirect to="/not-found"/>
-                </Switch>
+                <HashRouter basename="/">
+                    <Navbar user={this.state.user}/>
+                    <Switch>
+                        <Route path="/users/:id" component={SingleUser}/>
+                        <Route path="/users" component={Users}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/Register" component={Register}/>
+                        <Route path="/logout" component={Logout}/>
+                        <Protect component={Dashboard} path="/dashboard"/>
+                        <Route path="/dashboard" component={Dashboard}/>
+                        <Route path="/not-found" component={NotFound}/>
+                        <Redirect from="/customers" to="/users"/>
+                        <Route path="/" exact component={Home}/>
+                        <Redirect to="/not-found"/>
+                    </Switch>
+                </HashRouter>
             </>
         )
     }
